7434c8c533d12df8d5db48731ec4dd438ca22e30
Álvaro Cuesta <alvaro-cuesta@GitHub>
2019-11-30 20:05:31 +0100

    Add my inputs

---
+++ bin/download-test-input.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+require("dotenv").config();
+
+const fsp = require("fs").promises;
+const path = require("path");
+const { downloadInput } = require("../lib/advent/input");
+const { getAdventDays, getAdventYears } = require("../lib/advent/time");
+
+const makeInputIO = (year, day) => input => {
+  const yearDir = path.join(__dirname, "..", "lib", `${year}`);
+
+  return fsp
+    .mkdir(yearDir, {
+      recursive: true,
+      mode: 0o755
+    })
+    .then(() =>
+      fsp.writeFile(path.join(yearDir, `${day}.test.input`), input, {
+        encoding: "utf8",
+        mode: 0o644,
+        flag: "w"
+      })
+    );
+};
+
+const dayIO = (year, day, session) =>
+  downloadInput(year, day, session).then(makeInputIO(year, day));
+
+const yearIO = (year, session) =>
+  getAdventDays(year).flatMap(day => dayIO(year, day, session));
+
+const allIO = session =>
+  getAdventYears().flatMap(year => yearIO(year, session));
+
+const session = process.env.SESSION;
+
+Promise.all(allIO(session))
+  .then(() => {
+    console.log("Finished!");
+  })
+  .catch(err => {
+    console.error(err);
+  });
